cf1e1078312b830917c368b19711abcbc87ad4cf
Sahil Ganesh Bhingarde <bhingardesahil41@gmail.com>
2024-07-17 18:31:58 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+const heading = React.creatElement ("h1",{id:"head",class:"heading"},"Hello From TonY");
+    
+const root = ReactDom.creatRoot(document.getElementById("root"));
+
+root.render(heading);
